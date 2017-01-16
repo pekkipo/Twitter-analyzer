452fbc40380a7fbf5a5919b4b577e84b69122758
pekkipo <pekkipodev@gmail.com>
2017-01-16 16:47:37 +0300

    Initial commit

---
+++ project/constants.py
@@ -0,0 +1,8 @@
+# APP settings
+CONSUMER_KEY = 'Az7Mpq47f5i1xPwNkvT3huqzW'
+CONSUMER_SECRET = 'KDKl78wkDxS3KlUmxPmepDxarL1PGA8dtIbSinnawlQSLXww7p'
+
+# TWITTER API. Find here https://dev.twitter.com/oauth/reference/post/oauth/
+REQUEST_TOKEN_URL = 'https://api.twitter.com/oauth/request_token'
+ACCESS_TOKEN_URL = 'https://api.twitter.com/oauth/access_token'
+AUTHORIZATION_URL = 'https://api.twitter.com/oauth/authorize'
