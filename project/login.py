0fe6dd53869b4c3c597353a7524f62b9953c99f7
pekkipo <pekkipodev@gmail.com>
2017-01-16 17:14:51 +0300

    dict(request_result)

--- project/login.py
+++ project/login.py
@@ -16,6 +16,13 @@ if response.status != 200: # 200 is everything's fine
     print("An error occured when getting the request token from Twitter")
 
 
-request_token  = dict(urlparse.parse_qsl(content))
+request_token  = dict(urlparse.parse_qsl(content.decode('utf-8')))
 # content is in a format of query string parameter
 # we convert it into a dictionary
+# decode converts bytes to strings
+# this is indeed more readable then the original request result without dict convertion
+
+# Info
+# dict([('hi', '123'), ('hey', '456')])
+# dict is run on the list of tuples in a format key-value into a dictionary, which is handy
+
