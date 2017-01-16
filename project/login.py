1b51db03eb55c55c644072bd4e8abbcdc3d8db7d
pekkipo <pekkipodev@gmail.com>
2017-01-16 18:01:08 +0300

    Request/Access tokens

--- project/login.py
+++ project/login.py
@@ -22,6 +22,35 @@ request_token  = dict(urlparse.parse_qsl(content.decode('utf-8')))
 # decode converts bytes to strings
 # this is indeed more readable then the original request result without dict convertion
 
+
+# Steps afterwards
+# Press Sign up with twitter button on my web-site
+# user presses sign up on twitter web-site
+# Twitter sends back to my web-site/auth
+# I get the auth code + request token -> twitter -> access token
+
+
+# We are doing Pin auth. Just for learning purpose. We have a console application
+print("Go to the following web-site:")
+print("{}?oauth_token={}".format(constants.AUTHORIZATION_URL, request_token['oauth_token']))
+# {} will be replaced by two variables
+# request_token is a dictionary
+# user gets the pin from the web-page
+
+# User must type the pin
+oauth_verifier = input("What is the pin? ")
+
+token = oauth2.Token(request_token['oauth_token'], request_token['oauth_token_secret'])
+token.set_verifier(oauth_verifier)
+
+client = oauth2.Client(consumer, token)
+
+# We use this client to get an access token
+response, content = client.request(constants.ACCESS_TOKEN_URL, 'POST')
+access_token  = dict(urlparse.parse_qsl(content.decode('utf-8')))
+
+print(access_token)
+
 # Info
 # dict([('hi', '123'), ('hey', '456')])
 # dict is run on the list of tuples in a format key-value into a dictionary, which is handy
